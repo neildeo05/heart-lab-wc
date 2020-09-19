20e5cb8c8ce4792ff30be27084af63e5b3ae0a5b
neildeo05 <30543077+neildeo05@users.noreply.github.com>
2020-09-19 12:03:15 -0700

    Changed Readme to include picture

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Word Cloud based on multiple GoFundMe pages
 The file wc.py is a python script that uses the WordCloud library
+![Wordcloud](wc.png "Word Cloud")
--- scrape.py
+++ scrape.py
@@ -7,7 +7,7 @@ def takeLink(link):
     s = BeautifulSoup(page)
     cl="o-campaign-story mt-three-halves o-campaign-story--is-open"
 
-    with open('words.txt', 'w') as fp:
+    with open('gofund.txt', 'w') as fp:
 
         fp.write(str(s.findAll('div', attrs = {
             'class': 'o-campaign-story mt-three-halves'
@@ -17,9 +17,6 @@ def takeLink(link):
 
 f = open('gm.html').read()
 soup = BeautifulSoup(f)
-
-
-
 for link in soup.findAll('a', attrs = {'class':'campaign-tile-img--contain js-lazy'}): 
     print(link['href'])
     takeLink(link['href'])
